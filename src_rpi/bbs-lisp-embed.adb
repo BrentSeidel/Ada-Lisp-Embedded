81c5e78b36bfa53d8374e286a5f7b259b210f269
Brent Seidel <brentseidel@mac.com>
2021-08-12 12:05:31 -0700

    Minor updates to I2C initialization for Raspberry PI.

--- src_rpi/bbs-lisp-embed.adb
+++ src_rpi/bbs-lisp-embed.adb
@@ -73,12 +73,13 @@ package body BBS.lisp.embed is
       for i in mcp23017_found'range loop
          addr := BBS.embed.i2c.MCP23017.addr_0 + BBS.embed.addr7(i);
          temp := i2c_ptr.read(addr, BBS.embed.i2c.MCP23017.IOCON, err);
-         if err = BBS.embed.i2c.none then
+         if err = BBS.embed.i2c.NONE then
             MCP23017_info(i).configure(i2c_bus, addr, err);
             MCP23017_found(i) := True;
             put_line("MCP23017 Found at address " & Integer'image(Integer(addr)));
          else
-            put_line("MCP23017 Not found at address " & Integer'image(Integer(addr)));
+            put_line("MCP23017 Not found at address " & Integer'image(Integer(addr))
+                     & " - " & BBS.embed.i2c.err_code'Image(err));
             MCP23017_found(i) := False;
          end if;
       end loop;
